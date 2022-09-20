caa8ddbf6701755af793364d0027fed0cc02d6c6
Emerson Lebleu <emerson.lebleu@gmail.com>
2022-09-20 15:27:56 -0600

    Add Search function

--- app/src/main/java/com/emersonlebleu/academicscheduleapp/UI/ListView.java
+++ app/src/main/java/com/emersonlebleu/academicscheduleapp/UI/ListView.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.PopupMenu;
+import android.widget.TextView;
 
 import com.emersonlebleu.academicscheduleapp.Adapters.AssessmentAdapter;
 import com.emersonlebleu.academicscheduleapp.Adapters.CourseAdapter;
@@ -26,11 +27,14 @@ import java.util.List;
 
 public class ListView extends AppCompatActivity {
     String rootList;
+    TextView searchField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
+
+        searchField = findViewById(R.id.searchField);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -141,5 +145,119 @@ public class ListView extends AppCompatActivity {
         }
 
 
+    }
+
+    public void findBtnClick(View view) {
+        String searchCriteria = searchField.getText().toString();
+
+        if (rootList.equals("Term")){
+            RecyclerView recyclerView = findViewById(R.id.rootRecyclerView);
+            Repository repo = new Repository(getApplication());
+            List<Term> terms = repo.getAllTerms();
+            List<Term> termsSearched = new ArrayList<Term>();
+
+            for (Term term: terms){
+                if (term.getTitle().toLowerCase().contains(searchCriteria.toLowerCase())){
+                    termsSearched.add(term);
+                }
+            }
+
+            final TermAdapter adapter = new TermAdapter(this);
+
+            recyclerView.setAdapter(adapter);
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+            adapter.setTerms(termsSearched);
+            setTitle("All Terms");
+        } else if (rootList.equals("Course")){
+            RecyclerView recyclerView = findViewById(R.id.rootRecyclerView);
+            Repository repo = new Repository(getApplication());
+            List<Course> courses = repo.getAllCourses();
+            List<Course> coursesSearched = new ArrayList<Course>();
+
+            for (Course course: courses){
+                if (course.getTitle().toLowerCase().contains(searchCriteria.toLowerCase())){
+                    coursesSearched.add(course);
+                }
+            }
+
+            final CourseAdapter adapter = new CourseAdapter(this, "ListView");
+
+            recyclerView.setAdapter(adapter);
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+            adapter.setCourses(coursesSearched);
+            setTitle("All Courses");
+        } else if (rootList.equals("Assessment")) {
+            RecyclerView recyclerView = findViewById(R.id.rootRecyclerView);
+            Repository repo = new Repository(getApplication());
+
+            List<Objective> objectives = repo.getAllObjectiveAssessments();
+            List<Performance> performances = repo.getAllPerformanceAssessments();
+            List<Assessment> assessments = new ArrayList<>();
+            assessments.addAll(objectives);
+            assessments.addAll(performances);
+
+            List<Assessment> assessmentsSearched = new ArrayList<>();
+
+            for (Assessment assessment: assessments){
+                if (assessment.getTitle().toLowerCase().contains(searchCriteria.toLowerCase())){
+                    assessmentsSearched.add(assessment);
+                }
+            }
+
+            final AssessmentAdapter adapter = new AssessmentAdapter(this, "ListView");
+
+            recyclerView.setAdapter(adapter);
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+            adapter.setAssessments(assessmentsSearched);
+            setTitle("All Assessments");
+        }
+    }
+
+    public void allBtnClick(View view) {
+        searchField.setText("");
+
+        if (rootList.equals("Term")){
+            RecyclerView recyclerView = findViewById(R.id.rootRecyclerView);
+            Repository repo = new Repository(getApplication());
+            List<Term> terms = repo.getAllTerms();
+            final TermAdapter adapter = new TermAdapter(this);
+
+            recyclerView.setAdapter(adapter);
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+            adapter.setTerms(terms);
+            setTitle("All Terms");
+        } else if (rootList.equals("Course")){
+            RecyclerView recyclerView = findViewById(R.id.rootRecyclerView);
+            Repository repo = new Repository(getApplication());
+            List<Course> courses = repo.getAllCourses();
+            final CourseAdapter adapter = new CourseAdapter(this, "ListView");
+
+            recyclerView.setAdapter(adapter);
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+            adapter.setCourses(courses);
+            setTitle("All Courses");
+        } else if (rootList.equals("Assessment")) {
+            RecyclerView recyclerView = findViewById(R.id.rootRecyclerView);
+            Repository repo = new Repository(getApplication());
+
+            List<Objective> objectives = repo.getAllObjectiveAssessments();
+            List<Performance> performances = repo.getAllPerformanceAssessments();
+            List<Assessment> assessments = new ArrayList<>();
+            assessments.addAll(objectives);
+            assessments.addAll(performances);
+
+            final AssessmentAdapter adapter = new AssessmentAdapter(this, "ListView");
+
+            recyclerView.setAdapter(adapter);
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+            adapter.setAssessments(assessments);
+            setTitle("All Assessments");
+        }
     }
 }
